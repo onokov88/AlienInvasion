66040e8a91c97c12777d1d0f81d301c74ddd208b
onokov <onokov@gmail.com>
2022-10-19 18:29:17 +0300

    Increase the speed with new level

--- settings.py
+++ settings.py
@@ -9,17 +9,33 @@ class Settings:
         self.bg_color = (230, 230, 230)
 
         # Ship settings
-        self.ship_speed = 1.5
         self.ship_limit = 2
 
         # Bullet settings
-        self.bullet_speed = 1.5
         self.bullet_width = 300  # TODO set back to 3 when done testing
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
 
         # Alien settings
-        self.alien_speed = 1.0
         self.fleet_drop_speed = 100  # TODO set back to 10 when done testing
-        self.fleet_direction = 1  # 1 represents right, -1 represents left
+
+        # How quickly the game speeds up.
+        self.speedup_scale = 1.2
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """Initialize settings that change throughout the game."""
+        self.ship_speed = 1.51
+        self.bullet_speed = 3.0
+        self.alien_speed = 1.0
+
+        # fleet_direction of 1 represents right, -1 represents left
+        self.fleet_direction = 1
+
+    def increase_speed(self):
+        """Increase speed settings."""
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
