826fdda43c93444dff0a508173fabef9bb3875de
onokov <onokov@gmail.com>
2022-10-21 11:46:01 +0300

    Showing the current level

--- alien_invasion.py
+++ alien_invasion.py
@@ -76,6 +76,7 @@ class AlienInvasion:
             self.stats.reset_stats()
             self.stats.game_active = True
             self.sb.prep_score()
+            self.sb.prep_level()
 
             # Get rid of any remaining aliens and bullets.
             self.aliens.empty()
@@ -140,6 +141,10 @@ class AlienInvasion:
             self.bullets.empty()
             self._create_fleet()
             self.settings.increase_speed()
+
+            # Increase level.
+            self.stats.level += 1
+            self.sb.prep_level()
 
     def _create_fleet(self):
         """Create the fleet of aliens."""
